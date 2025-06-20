9cf70a162ae3f077d99a930591c16c7b9d4f5855
Mdjakaria53 <kjakaria53@gmail.com>
2025-06-20 22:20:39 +0900

    create hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HowItWorks from '../../components/HowItWorks/HowItWorks';
 import OurServices from '../../components/OurServices/OurServices';
+import HeroSection from '../../components/HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <div>
+            <HeroSection></HeroSection>
             <HowItWorks></HowItWorks>
             <OurServices></OurServices>
         </div>
