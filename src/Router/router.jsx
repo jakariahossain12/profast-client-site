720176028a98b928de4b2ca02b7d064ad5e61997
Mdjakaria53 <kjakaria53@gmail.com>
2025-06-21 14:38:15 +0900

    create a register page

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -4,20 +4,23 @@ import Home from "../pages/Home/Home";
 import CoverageMap from "../components/CoverageMap/CoverageMap";
 import AuthLayOut from "../layout/AuthLayOut";
 import SignIn from "../pages/UserAuthentiction/Sign_In/SignIn";
+import Register from "../pages/UserAuthentiction/Register/Register";
 export const router = createBrowserRouter([
   {
     path: "/",
-        Component: MainLayOut,
-        children: [
-          { index: true, Component: Home },
-          {path:'coverage-map',Component:CoverageMap,loader:()=>fetch('/warehouses.json')}
-    ]
+    Component: MainLayOut,
+    children: [
+      { index: true, Component: Home },
+      {
+        path: "coverage-map",
+        Component: CoverageMap,
+        loader: () => fetch("/warehouses.json"),
+      },
+    ],
   },
   {
-    path: '/',
+    path: "/",
     Component: AuthLayOut,
-    children: [
-      {path:'sign-in',Component:SignIn}
-    ]
-  }
+    children: [{ path: "sign-in", Component: SignIn }, { path: "register",Component:Register }],
+  },
 ]);
--- src/pages/UserAuthentiction/Register/Register.jsx
+++ src/pages/UserAuthentiction/Register/Register.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import WebsiteLogo from '../../../share/WebsiteLogo/WebsiteLogo';
 import Google from '../../../share/Social_Login/Google';
+import WebsiteLogo from '../../../share/WebsiteLogo/WebsiteLogo';
 import AuthButton from '../../../share/AuthButton/AuthButton';
 import { Link } from 'react-router';
 
-const SignIn = () => {
+const Register = () => {
+    const handle = (e) => {
+        e.preventDefault()
+    }
     return (
       <div className="md:w-1/2 flex flex-col justify-center items-center px-8 md:px-24 py-12 bg-white">
         <div className="w-full max-w-md">
           <div className="md:-mt-20 pb-20">
             <WebsiteLogo></WebsiteLogo>
           </div>
-          <h2 className="text-3xl font-bold mb-2">Welcome Back</h2>
-          <p className="text-sm text-gray-600 mb-6">Login with Profast</p>
+          <h2 className="text-3xl font-bold mb-2 text-center">Create an account</h2>
+          
 
-          <form className="space-y-4">
+          <form onSubmit={handle} className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium">Name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="mt-1 w-full border rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
             <div>
               <label className="block text-sm font-medium">Email</label>
               <input
@@ -33,16 +44,14 @@ const SignIn = () => {
               />
             </div>
 
-            <div className="text-right text-sm text-lime-600 cursor-pointer hover:underline">
-              Forget Password?
-            </div>
+            
 
-            <AuthButton lavle={"Login"}></AuthButton>
+            <AuthButton lavle={"Register"}></AuthButton>
 
             <p className="text-sm text-center">
-              Don’t have any account?{" "}
-              <Link to={'/register'} className="text-lime-600 font-medium cursor-pointer hover:underline">
-                Register
+              Have an account?{" "}
+              <Link to={'/sign-in'} className="text-lime-600 font-medium cursor-pointer hover:underline">
+                Login
               </Link>
             </p>
 
@@ -52,12 +61,11 @@ const SignIn = () => {
               <div className="h-px w-full bg-gray-300"></div>
             </div>
 
-                    <Google></Google>
-           
+            <Google></Google>
           </form>
         </div>
       </div>
     );
 };
 
-export default SignIn;
+export default Register;
